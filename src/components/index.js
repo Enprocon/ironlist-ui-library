fb0430e97af73ad067f826db5613d074b3cde175
Swaraj Gandhi <swaraj@browserstack.com>
2021-04-23 13:43:05 +0530

    added changes for the repo

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Card } from './Card';
+export { default as FloatingMenu } from './FloatingMenu';
+export { default as Icons } from './Icons';
+export { default as Input } from './Input';
+export { default as Slider } from './Slider';
+export { default as Tag } from './Tag';
--- src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Card } from './components/Card';
-export { default as FloatingMenu } from './components/FloatingMenu';
-export { default as Icons } from './components/Icons';
-export { default as Input } from './components/Input';
-export { default as Slider } from './components/Slider';
-export { default as Tag } from './components/Tag';
